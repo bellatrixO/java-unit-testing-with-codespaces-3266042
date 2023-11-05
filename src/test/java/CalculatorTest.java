532f163e45b5adb473990544a543ef0556587185
SlawomirRzaca <slawomir.rzaca@gmail.com>
2023-11-05 18:08:40 +0100

    02_03 result

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,8 +9,31 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         int expected = 3;
-
         int actual = calculator.add(1, 2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSubtraction() {
+        int expected = 0;
+        int actual = calculator.subtract(1, 1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMultiply() {
+        int expected = 10;
+        int actual = calculator.multiply(10, 1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivide() {
+        int expected = 15;
+        int actual = calculator.divide(30, 2);
 
         assertEquals(expected, actual);
     }
